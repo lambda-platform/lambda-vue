e5dda802aa344a4fd777a676e6f203111ffe303d
munkh-altai <munkaltai@gmail.com>
2023-02-17 11:22:58 +0900

    dayjs import bug fixed

--- src/modules/dataform/DataformMixin.js
+++ src/modules/dataform/DataformMixin.js
@@ -1,4 +1,3 @@
-import dayjs from 'dayjs'
 import {element} from './elements'
 import {getRule, setModel, setIdentity} from './rule'
 import {dataFromTemplate} from './utils/formula.js'
@@ -11,10 +10,12 @@ import {notification} from 'ant-design-vue';
 import fromFooter from './formFooter';
 import advancedFormat from 'dayjs/plugin/advancedFormat'
 import customParseFormat from 'dayjs/plugin/customParseFormat'
-import localeData from 'dayjs/plugin/localeData'
-import weekday from 'dayjs/plugin/weekday'
-import weekOfYear from 'dayjs/plugin/weekOfYear'
-import weekYear from 'dayjs/plugin/weekYear'
+
+import * as dayjs from 'dayjs';
+import * as localeData from 'dayjs/plugin/localeData'
+import * as weekday from 'dayjs/plugin/weekday'
+import * as weekOfYear from 'dayjs/plugin/weekOfYear'
+import * as weekYear from 'dayjs/plugin/weekYear'
 
 dayjs.extend(customParseFormat)
 dayjs.extend(advancedFormat)
