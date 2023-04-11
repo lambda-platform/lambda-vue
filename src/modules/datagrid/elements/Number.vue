4c3e55ff03f4b89cc313f99a681399941bba2df1
munkh-altai <munkaltai@gmail.com>
2023-04-11 13:58:50 +0800

    gir number number format checker added

--- src/modules/datagrid/elements/Number.vue
+++ src/modules/datagrid/elements/Number.vue
@@ -1,7 +1,7 @@
 <template>
   <span>
-        {{ params.value !== null ? Number(params.value).toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1,") : ''}}
-    </span>
+      {{ params.value !== null ? (typeof params.value === 'number' || !isNaN(params.value)) ? Number(params.value).toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1,") : params.value : ''}}
+  </span>
 </template>
 
 <script>
