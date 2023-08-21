0f1ba445571b67b00fe69f29deba7da99d8b22e3
munkh-altai <munkaltai@gmail.com>
2023-08-21 13:26:31 +0800

    dayjs updated

--- src/utils/dayjs.js
+++ src/utils/dayjs.js
@@ -5,13 +5,19 @@ import localeData from 'dayjs/esm/plugin/localeData/index'
 import weekday from 'dayjs/esm/plugin/weekday/index'
 import weekOfYear from 'dayjs/esm/plugin/weekOfYear/index'
 import weekYear from 'dayjs/esm/plugin/weekYear/index'
-
+import isoWeek from 'dayjs/esm/plugin/isoWeek'
+import updateLocale from 'dayjs/esm/plugin/updateLocale'
+import 'dayjs/locale/mn';
 dayjs.extend(customParseFormat)
 dayjs.extend(advancedFormat)
 dayjs.extend(weekday)
 dayjs.extend(localeData)
 dayjs.extend(weekOfYear)
 dayjs.extend(weekYear)
-
-
+dayjs.extend(isoWeek)
+dayjs.extend(updateLocale)
+dayjs.locale('mn');
+dayjs.updateLocale('mn', {
+    weekStart: 0
+})
 export default dayjs
