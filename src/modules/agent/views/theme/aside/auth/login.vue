960f7dbfcad82f6b9996ebfce3eedfef988a9a6f
munkh-altai <munkaltai@gmail.com>
2025-02-17 12:55:02 +0800

    removed duplicate message

--- src/modules/agent/views/theme/aside/auth/login.vue
+++ src/modules/agent/views/theme/aside/auth/login.vue
@@ -55,7 +55,7 @@
                 <div id="msg">
                     <span v-if="isSuccess" class="success">{{ lang.loginSuccess }}</span>
                     <span v-if="isError" class="error">{{ lang.loginError }}</span>
-                    <p v-if="errorMessage" class="error">{{ errorMessage }}</p>
+                    <p v-if="errorMessage && errorMessage !== 'Нэвтрэх нэр эсвэл нууц үг буруу байна'" class="error">{{ errorMessage }}</p>
 
                 </div>
 
